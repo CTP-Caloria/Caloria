6b4b4e8a1cf18e28ee43505e72b132541cc7fc85
Jessica <jchu0398@gmail.com>
2020-10-27 11:07:32 -0400

    Search results for recipes

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
+function SearchField(props) {
+    return (
+      <div className="text-center mt-4">
+        <input type="text" onChange={ props.changed } value={ props.value } />
+        {/* <p>You entered: { props.value }</p> */}
+      </div>);
+  }
+
 class SearchPage extends React.Component {
     state = {
         error: false,
@@ -16,7 +24,16 @@ class SearchPage extends React.Component {
     }
 
     SearchPost =(e) => {
-        fetch("")
+        this.setState({
+            content: e.target.value
+        })
+
+        fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + e.target.value)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+        })
+        .catch(error => {console.log(error)});
     }
 
 
@@ -27,14 +44,15 @@ class SearchPage extends React.Component {
             <div className="col-10 col-md-8 col-lg-7">
                 
                 <div className="input-group">
-                    <input
+                    <SearchField changed={ (e) => this.SearchPost(e) } value={ this.state.content } />
+                    {/* <input
                         type="text"
                         placeholder="Search..."
                         value={this.state.content}
                         className="form-control mr-3 rounded"
-                        onChange={this.contentChanged}
+                        changed={ (e) => this.SearchPost(e) }
                         
-                    />
+                    /> */}
                     <button className="btn btn-primary" onClick={this.savePost}>Search</button>
                 </div>
             </div>
