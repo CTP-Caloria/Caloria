b394abba70e826d06d6fdd44d1bf09fc2f1f449e
Jessica <jchu0398@gmail.com>
2020-11-16 18:46:32 -0500

    Create recipe page adds multiple steps and foods

--- client/src/pages/CalorieJournalPage.js
+++ client/src/pages/CalorieJournalPage.js
@@ -404,7 +404,7 @@ class CalorieJournalPage extends React.Component {
                                         name="units"
                                         onChange={this.handleChange.bind(this)}
                                     >
-                                        <option selected disabled hidden>Pick one</option>
+                                        <option selected disabled hidden>Choose...</option>
                                         <option value="Cups" >Cups</option>
                                         <option value="Grams">Grams</option>
                                         <option value="Ounces">Ounces</option>
--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import auth from '../services/auth';
 import { Redirect } from 'react-router-dom';
+import { BsPlus, BsX } from 'react-icons/bs';
 
 const axios = require('axios');
 
@@ -20,18 +21,117 @@ const formValid = ({ ...rest }) => {
 class CreatePage extends React.Component {
   state = {
     recipeName: "",
-    ingredients: "",
-    servingSize: "",
-    units: "",
+    ingredients: [{ food: "", servingSize: "", units: "" }],
+    instructions: [{ steps: "" }]
   }
 
-  handleChange = (e) => {
+  addInstruction = () => {
+    this.setState(prevState => ({
+      instructions: [...prevState.instructions, { steps: "" }]
+    }))
+  }
+
+  removeInstruction(i) {
+    let instructions = [...this.state.instructions];
+    instructions.splice(i,1);
+    this.setState({ instructions });
+  }
+
+  instructionInput() {
+    return this.state.instructions.map((x, i) => (
+      <div className="row align-items-center" key={i}>
+        <div className="form-group col">
+          <input 
+            type="text" 
+            className="form-control mt-1" 
+            id="steps" 
+            value={x.steps}
+            onChange={this.handleInstructionsChange.bind(this, i)}/>
+        </div>
+        <span className="col" type="button" value="remove" onClick={this.removeInstruction.bind(this, i)}><BsX /></span>
+      </div>
+    ))
+  }
+
+  handleInstructionsChange = (i, e) => {
     e.preventDefault();
     const { name, value } = e.target;
     console.log(name, value);
 
+    let instructions = [...this.state.instructions];
+    instructions[i] = {...instructions[i], [name]: value};
+
     this.setState({
-        [name]: value
+        instructions
+    }, () => console.log(this.state));
+
+    // console.log(this.state);
+  }
+
+  addIngredient = () => {
+    this.setState(prevState => ({
+      ingredients: [...prevState.ingredients, { food: "",  }]
+    }))
+  }
+
+  removeIngredient(i) {
+    let ingredients = [...this.state.ingredients];
+    ingredients.splice(i,1);
+    this.setState({ ingredients });
+  }
+
+  ingredientInput() {
+    return this.state.ingredients.map((x, i) => (
+      <div className="row align-items-center" key={i}>
+        <div className="form-group col">
+          <label className="form-label" htmlFor="food">Food</label>
+          <input 
+            type="text" 
+            className="form-control mt-1" 
+            id="food" 
+            value={x.food}
+            onChange={this.handleIngredientsChange.bind(this, i)}/>
+        </div>
+        <div className="form-group col">
+          <label className="form-label" htmlFor="servingSize">Serving Size</label>
+          <input 
+            type="number" 
+            min="0" 
+            className="form-control mt-1" 
+            id="servingSize"
+            value={x.servingSize} 
+            onChange={this.handleIngredientsChange.bind(this, i)}/>
+        </div>
+        <div className="form-group col">
+          <label className="form-label" htmlFor="unit">Unit</label>
+          <select 
+            className="form-control mt-1" 
+            value={x.units}
+            onChange={this.handleIngredientsChange.bind(this, i)}
+          >
+            <option selected disabled hidden>Choose one</option>
+            <option>Cups</option>
+            <option>Grams</option>
+            <option>Ounces</option>
+            <option>Teaspoons</option>
+            <option>Tablespoons</option>
+          </select>
+        </div>
+        <span className="col" type="button" value="remove" onClick={this.removeIngredient.bind(this, i)}><BsX /></span>
+      </div>
+    ))
+  }
+
+  handleIngredientsChange = (i, e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    console.log(name, value);
+
+    let ingredients = [...this.state.ingredients];
+    ingredients[i] = {...ingredients[i], [name]: value};
+
+    this.setState({
+        ingredients
     }, () => console.log(this.state));
 
     // console.log(this.state);
@@ -72,35 +172,7 @@ class CreatePage extends React.Component {
     } else {
         alert("Please log in to use this feature!");
     }
-}
-
-  // savePost = (event) => {
-  //   fetch("/api/posts/", {
-  //     method: 'POST',
-  //     credentials: 'include',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({content: this.state.content}),
-  //   })
-  //     .then(res => {
-  //       if(res.ok) {
-  //         return res.json()
-  //       }
-
-  //       throw new Error('Content validation');
-  //     })
-  //     .then(post => {
-  //       this.setState({
-  //         success: true,
-  //       });
-  //     })
-  //     .catch(err => {
-  //       this.setState({
-  //         error: true,
-  //       });
-  //     });
-  // }
+  }
 
   render() {
     if (auth.isAuthenticated) {
@@ -110,37 +182,33 @@ class CreatePage extends React.Component {
     }
 
     return (
-      <div className="form">
-        <div className="form-group row">
-          <label className="form-label" htmlFor="recipeName">Recipe Name</label>
-          <input type="text" className="form-control mt-1" id="recipeName" onChange={this.handleChange}/>
+      <div className="form w-50">
+        <h5 className="display-4 mt-3 mb-4 text-center">Create your recipe</h5>
+        <div className="form-group row mb-4">
+          <label className="form-label h6" htmlFor="recipeName">Recipe Name</label>
+          <input 
+            type="text" 
+            className="form-control mt-1" 
+            id="recipeName" 
+            placeholder="e.g. Banana bread"
+            onChange={this.handleIngredientsChange}/>
         </div>
 
-        <div className="row">
-          <div className="form-group col">
-            <label className="form-label" htmlFor="ingredients">Ingredients</label>
-            <input type="text" className="form-control mt-1" id="ingredients" onChange={this.handleChange}/>
-          </div>
-          <div className="form-group col">
-            <label className="form-label" htmlFor="amount">Amount</label>
-            <input type="number" min="0" className="form-control mt-1" id="amount" onChange={this.handleChange}/>
-          </div>
-          <div className="form-group col">
-            <label className="form-label" htmlFor="unit">Unit</label>
-            <select className="form-control mt-1" onChange={this.handleChange.bind(this)}>
-              <option selected disabled hidden>Choose one</option>
-              <option>Cups</option>
-              <option>Grams</option>
-              <option>Ounces</option>
-              <option>Teaspoons</option>
-              <option>Tablespoons</option>
-            </select>
-          </div>
-        </div>
+        <div className="row h6">Ingredients</div>
+        {this.ingredientInput()}
+        <button className="btn btn-secondary mb-4" value="add" onClick={this.addIngredient.bind(this)}>Add ingredients</button>
+          
+        <div className="form-group row h6">Instructions</div>
+        {this.instructionInput()}
+        <button className="btn btn-secondary mb-4" value="add" onClick={this.addInstruction.bind(this)}>Add instructions</button>  
           
         <div className="form-group row">
-          <label className="form-label" htmlFor="instructions">Instructions</label>
-          <input type="text" className="form-control mt-1" id="instructions" onChange={this.handleChange}/>
+          <label className="form-label h6" htmlFor="recipe-pic">Choose a picture for your recipe: </label>
+          <input
+            className="form-control mt-1 border-0" 
+            type="file"
+            id="recipe-pic" name="recipe-pic"
+            accept="image/png, image/jpeg" />
         </div>
 
         <div className="form-group row">
